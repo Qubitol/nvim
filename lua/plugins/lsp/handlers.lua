d08fcae0e9195e3a621ee8087a34cd23f8bb664b
Daniele Massaro <d.massaro.26@gmail.com>
2023-02-23 01:05:21 +0100

    Modify keymaps to enter loclist and qflist

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-local status_trouble_ok, _ = pcall(require, "trouble")
-
 M.setup = function()
     local signs = {
         { name = "DiagnosticSignError", text = "" },
@@ -67,15 +65,15 @@ local function lsp_keymaps(bufnr)
     keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>zz", opts)
     keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
     keymap(bufnr, "n", "<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    if status_trouble_ok then
-        keymap(bufnr, "n", "gr", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
-        keymap(bufnr, "n", "<leader>ll", "<cmd>TroubleToggle loclist<CR>", opts)
-        keymap(bufnr, "n", "<leader>lq", "<cmd>TroubleToggle quickfix<CR>", opts)
-    else
-        keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-        keymap(bufnr, "n", "<leader>ll", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-        keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setqflist()<CR>", opts)
-    end
+    -- if status_trouble_ok then
+    --     keymap(bufnr, "n", "gr", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
+    --     keymap(bufnr, "n", "<leader>tl", "<cmd>TroubleToggle loclist<CR>", opts)
+    --     keymap(bufnr, "n", "<leader>tq", "<cmd>TroubleToggle quickfix<CR>", opts)
+    -- else
+    keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    keymap(bufnr, "n", "<leader>tl", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+    keymap(bufnr, "n", "<leader>tq", "<cmd>lua vim.diagnostic.setqflist()<CR>", opts)
+    -- end
 end
 
 M.on_attach = function(client, bufnr)
