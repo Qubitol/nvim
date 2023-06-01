f4e34fec8dbffaa0b7be0d4bee92650d1a01cbf4
Daniele Massaro <d.massaro.26@gmail.com>
2023-06-02 01:34:26 +0200

    Set tab to work also within snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -183,24 +183,22 @@ cmp.setup({
         }),
         -- Multi-purpose Tab
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-                -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-                -- they way you will only jump inside the snippet region
-            elseif luasnip.expand_or_jumpable() then
+            if luasnip.expand_or_locally_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
+            elseif cmp.visible() then
+                cmp.select_next_item()
+            -- elseif has_words_before() then
+            --     cmp.complete()
             else
                 fallback()
             end
         end, { "i", "s" }),
         -- Multi-purpose Shift+Tab
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+            if luasnip.locally_jumpable(-1) then
                 luasnip.jump(-1)
+            elseif cmp.visible() then
+                cmp.select_prev_item()
             else
                 fallback()
             end
