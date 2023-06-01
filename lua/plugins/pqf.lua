b54cd29cfeb347667b2e0548df1b20231a56e459
Daniele Massaro <d.massaro.26@gmail.com>
2023-06-01 02:07:07 +0200

    Update diagnostic symbols

--- lua/plugins/pqf.lua
+++ lua/plugins/pqf.lua
@@ -5,10 +5,10 @@ end
 
 pqf.setup({
 	signs = {
-        error = "",
-        warning = "",
-        info = "",
-        hint = "",
+        error = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
+        warning = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
+        info = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
+        hint = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
 	},
 	show_multiple_lines = false,
 })
