e1c942dd241f0cb951fb87ed23af64b9782132b8
Daniele Massaro <d.massaro.26@gmail.com>
2023-10-09 00:58:50 +0200

    Fix heirline crashing if opening vim without any file

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -362,14 +362,11 @@ return {
 
             condition = conditions.has_diagnostics,
 
-            static = {
-                error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
-                warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
-                info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
-                hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
-            },
-
             init = function(self)
+                self.error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text
+                self.warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text
+                self.info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text
+                self.hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text
                 self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
                 self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
                 self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
