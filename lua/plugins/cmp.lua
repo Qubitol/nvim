93cfaadef768827f7d1888cf674844fa29539e10
Daniele Massaro <d.massaro.26@gmail.com>
2023-10-08 23:23:30 +0200

    Add CmdlineChanged to the list of events lazy loading cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,7 +1,7 @@
 return {
     "hrsh7th/nvim-cmp",
     version = false, -- last release is way too old
-    event = "InsertEnter",
+    event = { "InsertEnter", "CmdlineChanged" },
     dependencies = {
         "L3MON4D3/LuaSnip",
         "hrsh7th/cmp-nvim-lsp",
