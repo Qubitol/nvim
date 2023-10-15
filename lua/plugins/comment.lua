b8297a849ec8f8641dfdd091dea0f700af128059
Daniele Massaro <d.massaro.26@gmail.com>
2023-10-15 02:07:34 +0200

    Update plugins and disable context commentstring that seems to not work right now

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -7,6 +7,6 @@ return {
         "JoosepAlviste/nvim-ts-context-commentstring",
     },
     opts = {
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+        -- pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
     },
 }
