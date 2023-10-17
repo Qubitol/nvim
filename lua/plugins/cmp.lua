802c0b057d1379c11fedd8a0d3cdc98464c0cdf6
Daniele Massaro <d.massaro.26@gmail.com>
2023-10-17 20:48:05 +0200

    Make copilot work well with cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -221,7 +221,7 @@ return {
             }),
 
             experimental = {
-                ghost_text = true,
+                ghost_text = false,
             },
         }
     end,
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -14,25 +14,26 @@ return {
                 auto_trigger = true,
                 keymap = {
                     accept = "<C-y>",
-                    next = "<C-n>",
-                    prev = "<C-p>",
+                    next = "<C-Tab>",
+                    prev = "<C-S-Tab>",
                     dismiss = "<C-e>",
                 },
             },
         })
 
-        -- hide copilot suggestions when cmp menu is open
-        -- to prevent odd behavior/garbled up suggestions
-        local cmp_status_ok, cmp = pcall(require, "cmp")
-        if cmp_status_ok then
-            cmp.event:on("menu_opened", function()
-                vim.b.copilot_suggestion_hidden = true
-            end)
-
-            cmp.event:on("menu_closed", function()
-                vim.b.copilot_suggestion_hidden = false
-            end)
-        end
+        -- -- hide copilot suggestions when cmp menu is open
+        -- -- to prevent odd behavior/garbled up suggestions
+        -- -- but disabling ghost text is a better solution
+        -- local cmp_status_ok, cmp = pcall(require, "cmp")
+        -- if cmp_status_ok then
+        --     cmp.event:on("menu_opened", function()
+        --         vim.b.copilot_suggestion_hidden = true
+        --     end)
+        --
+        --     cmp.event:on("menu_closed", function()
+        --         vim.b.copilot_suggestion_hidden = false
+        --     end)
+        -- end
     end,
 
     keys = function()
