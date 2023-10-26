b8ab24f1b924ea4573976a428ebf60b3ee1baa6e
Daniele Massaro <d.massaro.26@gmail.com>
2023-10-26 23:14:41 +0200

    Add cfilter builtin plugin

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -2,6 +2,9 @@ local set = vim.opt_local
 local map = vim.keymap.set
 local opts = { buffer = true, noremap = true, silent = true }
 
+-- load cfilter-plugin
+vim.cmd [[packadd cfilter]]
+
 -- preview the diagnostic location
 map("n", "p", "<CR>zz<C-w>p", opts)
 map("n", "J", "j<CR>zz<C-w>p", opts)
