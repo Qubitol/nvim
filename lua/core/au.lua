9343a8485d9f060cd765396da65a980d59de2f68
Daniele Massaro <d.massaro.26@gmail.com>
2024-06-27 23:11:06 +0200

    Keep only relevant autocommands

--- lua/core/au.lua
+++ lua/core/au.lua
@@ -17,34 +17,6 @@ autocmd("TextYankPost", {
     end,
 })
 
--- Filetypes autocommands
-local ft_group = augroup("FileTypeGroup", {})
-
-autocmd("FileType", {
-    group = ft_group,
-    pattern = "TelescopePrompt",
-    callback = function()
-        local status, cmp = pcall(require, "cmp")
-        if not status or not cmp.setup then
-            return
-        end
-        cmp.setup.buffer({
-            enabled = false,
-        })
-    end,
-})
-
--- DAP autocommands
-local dap_group = augroup("DAPGroup", {})
-
-autocmd("BufWinEnter", { -- also BufEnter works
-    group = dap_group,
-    pattern = "DAP *,\\[dap-repl\\]",
-    callback = function()
-        vim.opt_local.cursorline = false
-    end,
-})
-
 -- Autosave on FocusLost
 local autosave_group = augroup("AutosaveGroup", {})
 
@@ -62,14 +34,4 @@ local lastplace_group = augroup("nvim-lastplace", {})
 autocmd({ "BufWinEnter", "FileType" }, {
     group = lastplace_group,
     callback = utils.goto_lastplace,
-})
-
--- Resize
-local resize_group = augroup("ResizeGroup", {})
-
-autocmd("VimResized", {
-    group = resize_group,
-    callback = function()
-        vim.cmd("tabdo wincmd =")
-    end,
 })
