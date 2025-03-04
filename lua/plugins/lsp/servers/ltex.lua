4b721e46173970d912a3ddf8ddc315f18ee812ec
Daniele Massaro <d.massaro.26@gmail.com>
2025-03-04 21:02:59 +0100

    Update lsp servers settings

--- lua/plugins/lsp/servers/ltex.lua
+++ lua/plugins/lsp/servers/ltex.lua
@@ -6,7 +6,22 @@ return {
             path = vim.fn.expand("~") .. "/.config/nvim/spell",
         })
     end,
+    filetypes = { "bib", "gitcommit", "org", "rst", "rnoweb", "tex", "pandoc", "quarto", "rmd", "context", "mail" },
     settings = {
-        ltex = {},
+        ltex = {
+            enabled = {
+                "bibtex",
+                "gitcommit",
+                "org",
+                "tex",
+                "restructuredtext",
+                "rsweave",
+                "latex",
+                "quarto",
+                "rmd",
+                "context",
+                "mail",
+            },
+        },
     },
 }
