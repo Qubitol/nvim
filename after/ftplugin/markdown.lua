f81d6fbf4e62a7ac740481ca3d4d94075fada74e
Daniele Massaro <d.massaro.26@gmail.com>
2025-06-07 00:56:32 +0100

    Add keymap to align tables

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,6 +1,10 @@
 local set = vim.opt_local
+local map = vim.keymap.set
+local opts = { buffer = true, noremap = true, silent = true }
 
 set.colorcolumn = ""
 set.wrap = true
 set.concealcursor = "nc"
 set.cursorlineopt = "number"
+
+map("v", "<leader>tf", "<cmd>'<,'>! tr -s ' ' | column -t -s '|' -o '|'<CR>", opts)
