364e1ce94f4ee1cc665b2056d1462c2ce7a06993
Daniele Massaro <d.massaro.26@gmail.com>
2026-04-18 23:04:00 +0200

    New session creation keymap

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,5 +1,9 @@
 -- Session
 
+local map = require("config.utils").map
+
 vim.pack.add({
     { src = "https://github.com/tpope/vim-obsession.git" },
 })
+
+map("n", "<leader>so", "<cmd>Obsession<CR>", "[S]ession tracking [O]n")
