6036ee7605531eb2880c3e82ad2ab0ae4c9932b9
Daniele Massaro <d.massaro.26@gmail.com>
2026-04-29 18:08:56 +0200

    Display images only when hovering

--- lua/plugins/wiki.lua
+++ lua/plugins/wiki.lua
@@ -36,9 +36,9 @@ if vim.g.pretty and is_kitty() then
         integrations = {
             markdown = {
                 enabled = true,
-                clear_in_insert_mode = false,
+                clear_in_insert_mode = true,
                 download_remote_images = true,
-                only_render_image_at_cursor = false,
+                only_render_image_at_cursor = true,
             },
             neorg = { enabled = false },
             typst = { enabled = false },
